d1043d05f23efc25382de104532f86fae7e7102e
Shivam Shandilya <70915754+legitShivam@users.noreply.github.com>
2022-09-11 13:16:26 +0530

    changed zsh-setup

---
+++ 03-application-setup/01-zsh-setup/02-zsh-config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## Installing plugins
+# Zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+
+# restoring dotifles
+. ../../01-config/05-dotfiles.sh
