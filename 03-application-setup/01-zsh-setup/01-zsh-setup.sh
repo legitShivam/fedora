033c84bcd244673e1ae44e681d58cdc36634b71b
Shivam Shandilya <70915754+legitShivam@users.noreply.github.com>
2022-09-18 14:41:35 +0530

    changed zsh setup

--- 03-application-setup/01-zsh-setup/01-zsh-setup.sh
+++ 03-application-setup/01-zsh-setup/01-zsh-setup.sh
@@ -12,22 +12,9 @@ dependencies="git zsh"
 sudo $package_manager install $dependencies -y
 
 
-# ohmyzsh[deprecated]
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# p10k theme for ohymyzsh
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-sudo sed -i 's\ZSH_THEME="robbyrussell"\ZSH_THEME="powerlevel10k/powerlevel10k"\g' ~/.zshrc
-
-
 # removing temp folder
 cd ..
 rm -rf ./temp
 
-
-# Rebooting zsh
-exec zsh
-
+# ohmyzsh[deprecated]
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
--- 03-application-setup/01-zsh-setup/02-zsh-config.sh
+++ 03-application-setup/01-zsh-setup/02-zsh-config.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+
+# p10k theme for ohymyzsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+sudo sed -i 's\ZSH_THEME="robbyrussell"\ZSH_THEME="powerlevel10k/powerlevel10k"\g' ~/.zshrc
+
+
+# Rebooting zsh
+exec zsh
 ## Installing plugins
 # Zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
